54bc5da369bb81cbfbd7a87dd7afd6f2985c2e51
Maxime Lalo <maxime.lalo.pro@gmail.com>
2024-10-06 00:31:28 +0200

    :sparkles: Your library working

--- frontend/src/components/pages/CreatedPage/index.tsx
+++ frontend/src/components/pages/CreatedPage/index.tsx
@@ -1,5 +1,54 @@
+import SongsList from "../../elements/SongsList";
 import PageTemplate from "../PageTemplate";
+import classes from "./classes.module.scss";
 
 export default function CreatedPage() {
-    return <PageTemplate tabTitle="Created"></PageTemplate>;
+    return (
+        <PageTemplate tabTitle="Created">
+            <div className={classes["root"]}>
+                <div className={classes["title"]}>Your library</div>
+                <div className={classes["songs-container"]}>
+                    <SongsList
+                        songs={[
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                            {
+                                artist: "The Weeknd",
+                                title: "Blinding Lights",
+                                image: "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
+                            },
+                        ]}
+                    />
+                </div>
+            </div>
+        </PageTemplate>
+    );
 }
